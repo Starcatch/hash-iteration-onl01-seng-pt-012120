b3c16ae1f30efdb13f904adfb80bccc3d5ecd79c
Timur Mishiev <timurmishiev@gmail.com>
2020-02-01 22:27:17 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,5 +11,18 @@ def happy_birthday(birthday_kids)
   end
 end
 
+birthday_kids = {
+  "Timmy" => 9,
+  "Sarah" => 6,
+  "Amanda" => 27
+}
+
+def age_appropriate_birthday(birthday_kids)
+  birthady_kids.each do |kids_name, age|
+    if age < 12
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end 
+end 
+  
 
 
